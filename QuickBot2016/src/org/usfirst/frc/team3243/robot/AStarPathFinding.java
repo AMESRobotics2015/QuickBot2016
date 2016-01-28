13707477b918568f8b388d5414b60a97861423f7
riley <riley@PCR-6000>
2016-01-28 16:24:02 -0700

    Finished the field setup part of my pathfinding program

--- QuickBot2016/src/org/usfirst/frc/team3243/robot/AStarPathFinding.java
+++ QuickBot2016/src/org/usfirst/frc/team3243/robot/AStarPathFinding.java
@@ -4,13 +4,34 @@ public class AStarPathFinding {
 
 	int l;
 	int w;
-	int[][] feild = new int[54][26];
+	int[][] field = new int[30][26];
 	
-	public void feildsetup(){
-		for(int x = 1 ; x < 55 ; x++){
-			for(int w = 1; w < 26; w++){
-				feild[l][w] = 1;
+	public void fieldsetup(){
+		for(int x = 1 ; x < 31 ; x++){
+			for(int w = 1; w < 27; w++){
+				field[l][w] = 1;
 			}
 		}
+		for (int l = 17; l < 21; l++){
+			for (int w = 1; w < 4; w++){
+				field[l][w] = 2;
+			}
+		}
+		for (int l = 17; l < 21; l++){
+			for (int w = 4; w < 22; w++){
+				field[l][w] = 3;
+			}
+		}
+		for (int l = 1; l < 25; l++){
+			for (int w = 23; w < 27; w++){
+				field[l][w] = 0;
+			}
+		}
+		field[3][9] = 6;
+		field[24][2] = 5;
+	}
+	
+	public void pathfind(){
+		
 	}
 }
